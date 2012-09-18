18643c84df0becf4232cd93a944c074de5d3a4fb
Marc Rendl Ignacio <boracay+marc@aelogica.com>
2012-09-18 11:46:04 +0800

    Added ability to add a configuration block by using SparkMotion::OAuth2Client#configure method
    - also made SparkMotion module smaller and more organized.

--- app/lib/spark_motion.rb
+++ app/lib/spark_motion.rb
@@ -1,7 +1,14 @@
+# As a 3rd party developer
+# To establish access with the Spark API
+# I should be able to do the ff:
+# - connection
+# - authentication
+# - request
 module SparkMotion
   VERSION = "0.1.0"
 
-  module Configuration
+  class OAuth2Client
+
     VALID_OPTION_KEYS = [
       :api_key,
       :api_secret,
@@ -11,58 +18,69 @@ module SparkMotion
       :callback,
       :user_agent,
       :version,
-      :ssl,
-      :oauth2_provider,
-      :authentication_mode
-    ].freeze
+      :ssl
+    ]
 
-    DEFAULT_API_KEY = nil
-    DEFAULT_API_SECRET = nil
-    DEFAULT_API_USER = nil
-    DEFAULT_ENDPOINT = 'https://api.sparkapi.com'
-    DEFAULT_AUTH_ENDPOINT = 'https://sparkplatform.com/openid'  # Ignored for Spark API Auth
-    DEFAULT_VERSION = 'v1'
-    DEFAULT_USER_AGENT = "Spark API RubyMotion Gem #{VERSION}"
-    DEFAULT_SSL = true
-    DEFAULT_OAUTH2 = nil
+    # keys that are usually updated from Spark in order to access their API
+    ACCESS_KEYS = [
+      :access_code,
+      :access_token,
+      :refresh_token,
+      :expires_in
+    ]
+
+    attr_accessor *VALID_OPTION_KEYS
+    attr_accessor *ACCESS_KEYS
+
+    DEFAULT = {
+      api_key: nil,
+      api_secret: nil,
+      api_user: nil,
+      endpoint: 'https://api.sparkapi.com',
+      auth_endpoint: 'https://sparkplatform.com/oauth2',  # Ignored for Spark API Auth
+      version: 'v1',
+      user_agent: "Spark API RubyMotion Gem #{VERSION}",
+      ssl: true
+    }
 
     X_SPARK_API_USER_AGENT = "X-SparkApi-User-Agent"
 
-    attr_accessor *VALID_OPTION_KEYS
+    def initialize opts={}
+      puts "#{self} initializing..."
+      VALID_OPTION_KEYS.each do |key|
+        send("#{key.to_s}=", DEFAULT[key])
+        puts "sent #{key.to_s} a default value"
+      end
+    end
 
+    # Sample Usage:
+    # client = SparkMotion::OAuth2Client.new
+    # client.configure do |config|
+    #   config.api_key      = "e8cd72745paowh67h1lygapif"
+    #   config.api_secret   = "2d692kqxipv0o9yxovyp6s98b"
+    #   config.callback     = "https://sparkplatform.com/oauth2/callback"
+    #   config.auth_endpoint = "https://sparkplatform.com/oauth2" # different for hybrid
+    #   config.endpoint   = 'https://developers.sparkapi.com'
+    # end
     def configure
       yield self
-    end
-
-    def reset_configuration
-      self.api_key     = DEFAULT_API_KEY
-      self.api_secret  = DEFAULT_API_SECRET
-      self.api_user    = DEFAULT_API_USER
-      self.authentication_mode = SparkApi::Authentication::ApiAuth
-      self.auth_endpoint  = DEFAULT_AUTH_ENDPOINT
-      self.endpoint    = DEFAULT_ENDPOINT
-      self.oauth2_provider = DEFAULT_OAUTH2
-      self.user_agent  = DEFAULT_USER_AGENT
-      self.ssl         = DEFAULT_SSL
-      self.version     = DEFAULT_VERSION
       self
     end
-  end
 
-  # this is the only way to authenticate for now
-  # As a developer
-  # To be able to authenticate this app to Spark API using OAuth2
-  # I should be able to use OAuth2 module
-  module OAuth2
-  end
+    def get_user_permission
+      # app opens safari and waits for a callback?code=<access_code>
+      # <access_code> is then assigned to client.access_code
+    end
 
-  # As a developer
-  # To sustain a connection with the Spark API
-  # I should be able use a Client module
-  # - connection
-  # - authentication
-  # - request
+    def authorize
+      # do a post request containing code..
+      # expect values to be assigned to ACCESS_KEYS attributes
+      # also add extra setup here
+    end
 
-  module Client
+    def get # Future TODO: post, put
+      # authorize if not authorized
+      # do get request via BW or Cocoa # check which is better
+    end
   end
 end
